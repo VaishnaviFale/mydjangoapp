655afbafd8100733be87897a36229b1439bdce4a
Vaishnavi Fale <88130356+VaishnaviFale@users.noreply.github.com>
2023-12-29 18:27:58 +0530

    Include Metrics Endpoint in Project URLs

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
 # myapp/urls.py
 from django.urls import path
 from .views import hello_world
+from .views import MyView
 
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
+    path('metrics/', include('myapp.metrics')),
 ]
 
